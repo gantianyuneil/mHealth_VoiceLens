e6101607da91d798f3a554c95479080004dba9c2
Alientton <yanganton024@gmail.com>
2022-12-25 01:13:40 -0500

    details in update diary

--- backend/v0/urls.py
+++ backend/v0/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.toLogin_view)
+    path('', views.toIndex_view),
+    path('login/', views.toLogin_view),
+    path('index/', views.Login_view),
 
 ]
--- backend/v0/views.py
+++ backend/v0/views.py
@@ -1,6 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
+# function directs to homepage
+def toIndex_view(request):
+    return render(request, 'index.html')
+
+# function directs to login page
 def toLogin_view(request):
     return render(request, 'login.html')
+
+#function directs to the page after login
+def Login_view(request):
+    # if "user" or "pwd" is not found, return a empty string
+    username = request.POST.get("user", '')
+    password = request.POST.get("pwd", '')
+
+    if username and password:
+        return HttpResponse("Login Successful!")
+    else:
+        return HttpResponse("Login Failed!")
